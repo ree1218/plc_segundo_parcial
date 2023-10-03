bddac6a698ffa6a69cb3d53a81464d3a2fca4305
Regina Navarro Álvarez <145035896+ree1218@users.noreply.github.com>
2023-10-02 18:30:31 -0600

    Create condicionales01.py

---
+++ condicionales01.py
@@ -0,0 +1,9 @@
+#2
+num=float(input("Dame un número:  "))
+if num>=1:
+  print("El numero", num, "es positivo")
+elif num<=-1:
+  print("El numero", num, "es negativo")
+else:
+  num=0
+  print("Tu numero es cero")
