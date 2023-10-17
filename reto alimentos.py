d07b54390b4e8131f09ee89cfc770c3dab319bda
Regina Navarro Álvarez <145035896+ree1218@users.noreply.github.com>
2023-10-17 08:55:08 -0600

    Create reto alimentos.py

---
+++ reto alimentos.py
@@ -0,0 +1,21 @@
+#https://replit.com/@A00574058/Reto-Evaluacion-de-alimentos
+#Reto- evaluación de alimentos.
+print ("Aqui evaluaremos los contenidos de azúcar, grasa y sodio de tus alimentos")
+print("Porfavor, inserta en gramos el contenido de grasa y azucar")
+
+grasa = float (input(""))
+azucar = float (input(""))
+
+if grasa <= 5 and azucar <= 10:
+  print("Tu alimento es bajo en grasa y azucar")
+
+print ("Ingresa en gramos el contenido de sodio")
+sodio = float (input(""))
+  
+if sodio <= 100:
+  print("Tu alimento es bajo en sodio")
+  
+else:
+  print("Considera las advertencias nutricionales de tu alimento")
+
+  
